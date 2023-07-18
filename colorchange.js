d115b4b8d67c931b3051314b03a03a8f4656f01b
sandip9938 <sandippanda577@gmail.com>
2023-07-18 20:51:30 +0530

    First Commit

---
+++ colorchange.js
@@ -0,0 +1,24 @@
+// Generate random color
+const randomcolor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+const startchangingcolor = function () {
+  intervalId = setInterval(changebgcolor, 1000);
+  function changebgcolor() {
+    document.body.style.backgroundColor = randomcolor();
+  }
+};
+const stopchangingcolor = function () {
+  clearInterval(intervalId);
+};
+
+document.getElementById("start").addEventListener("click", startchangingcolor);
+
+document.getElementById("stop").addEventListener("click", stopchangingcolor);
